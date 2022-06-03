c6fe11b362957b441eb1a122163be096cfd0d14a
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-03 13:46:04 +0200

    javascript_course

--- section 1/starter/script.js
+++ section 1/starter/script.js
@@ -1,28 +1,49 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-console.log('Jonas');
-console.log(23);
+// console.log('Jonas');
+// console.log(23);
 
-let firstName = 'Matilda';
+// let firstName = 'Matilda';
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let jonas_matilda = 'JM';
-let $function = 27;
+// let jonas_matilda = 'JM';
+// let $function = 27;
 
-let person = 'jonas';
-let PI = 3.1415;
+// let person = 'jonas';
+// let PI = 3.1415;
 
-let myFirstJob = 'Programmer';
-let myCurrentJob = 'Teacher';
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
 
-let country = 'Serbia';
-let continent = 'Europe';
-let population = 10000000;
+// let country = 'Serbia';
+// let continent = 'Europe';
+// let population = 10000000;
 
-console.log(country, continent, population);
+// console.log(country, continent, population);
+
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
