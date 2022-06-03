bd64fbc45634dcbee31f8fff3dcc80c5eec68497
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-03 18:56:27 +0200

    javascript_course

--- section 1/starter/script.js
+++ section 1/starter/script.js
@@ -27,23 +27,72 @@
 
 // console.log(country, continent, population);
 
-let javascriptIsFun = true;
+// let javascriptIsFun = true;
 
-console.log(javascriptIsFun);
+// console.log(javascriptIsFun);
 
 // console.log(typeof true);
-console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 // console.log(typeof 23);
-// console.log(typeof 'Jonas');
+//  console.log(typeof 'Jonas');
 
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// lastName = 'Schmedtmann';
+// console.log(lastName);
+
+// // Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// const ageJovan = now - 2001;
+// console.log(ageJonas, ageSarah, ageJovan);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// //  2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+
+// //  Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// console.log(x);
+
+// // Comparison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
