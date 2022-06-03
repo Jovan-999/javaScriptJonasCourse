d096063b9a44b2bdba0bddbfe6f44a3460131a47
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-03 13:21:07 +0200

    javascript_course

--- section 1/starter/script.js
+++ section 1/starter/script.js
@@ -1,5 +1,28 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is fun');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
+
+let country = 'Serbia';
+let continent = 'Europe';
+let population = 10000000;
+
+console.log(country, continent, population);
