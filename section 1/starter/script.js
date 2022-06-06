14c37fa7357b54e9b5e68d52ecee00f5c4a4f7d4
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-06 18:56:23 +0200

    javascript_course

--- section 1/starter/script.js
+++ section 1/starter/script.js
@@ -121,6 +121,9 @@
 // const markHigherBMI = BMIMark > BMIJohn;
 // console.log(BMIMark, BMIJohn, markHigherBMI);
 
+/////////////////////////////////////////////////
+// STRINGS AND TEMPLATE LITERALS
+
 // const firstName = 'Jonas';
 // const job = 'teacher';
 // const birthYear = 1991;
@@ -166,3 +169,23 @@
 // console.log(century);
 
 // CODING CHALLANGE #2
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log(BMIMark, BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`);
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
+// }
