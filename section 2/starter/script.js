e52e131470b9d467615b45035dc8e5b08148ff7d
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-07 19:12:01 +0200

    javascript_course

--- section 2/starter/script.js
+++ section 2/starter/script.js
@@ -3,12 +3,37 @@
 ///////////////////////////////////////
 // Activating Strict Mode
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive');
 
 // const interface = 'Audio';  RESERVED WORDS
 // const private = 534; RESERVED WORDS
 // const if = 23; RESERVED WORDS
+
+///////////////////////////////////////
+// Functions
+
+// function logger() {
+//   console.log('My name is Jonas');
+// }
+// calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+// const num = Number('23');
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
