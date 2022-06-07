f1a26a5706b130bb351b75d82a98c5f185ce0418
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-07 15:58:36 +0200

    javascript_course

---
+++ section 2/starter/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+///////////////////////////////////////
+// Activating Strict Mode
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// const interface = 'Audio';  RESERVED WORDS
+// const private = 534; RESERVED WORDS
+// const if = 23; RESERVED WORDS
