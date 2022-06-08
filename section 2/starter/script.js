38389d568b938c0ff51bc46db15d608c9b89ce8b
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-08 20:18:33 +0200

    javascript_course

--- section 2/starter/script.js
+++ section 2/starter/script.js
@@ -507,17 +507,17 @@ GOOD LUCK 😀
 // for (let rep = 1; rep <= 10; rep++) {
 //   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 // }
-let rep = 1;
-while (rep <= 10) {
-  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
-  rep++;
-}
-let dice = Math.trunc(Math.random() * 6) + 1;
-while (dice !== 6) {
-  console.log(`You rolled a ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
-  if (dice === 6) console.log('Loop is about to end...');
-}
+// let rep = 1;
+// while (rep <= 10) {
+//   // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+//   rep++;
+// }
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log('Loop is about to end...');
+// }
 
 ///////////////////////////////////////
 // Coding Challenge #4
@@ -535,3 +535,28 @@ HINT: Call calcTip in the loop and use the push method to add values to the tips
   4.3. Call the function with the 'totals' array
 GOOD LUCK 😀
 */
+
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+
+// for (let i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
+// console.log(bills, tips, totals);
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     // sum = sum + arr[i];
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// };
+// console.log(calcAverage([2, 3, 7]));
+// console.log(calcAverage(totals));
+// console.log(calcAverage(tips));
