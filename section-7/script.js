b76ab03aed737efada1284d783cd134080db5065
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-23 11:12:31 +0200

    javascript_course

--- section-7/script.js
+++ section-7/script.js
@@ -50,6 +50,19 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Logical Assignment Operators
+
+///////////////////////////////////////
+// The Nullish Coalescing Operator
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+// // Nullish: null and undefined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
+
+///////////////////////////////////////
 // Short Circuiting (&& and ||)
 
 // console.log('---- OR ----');
