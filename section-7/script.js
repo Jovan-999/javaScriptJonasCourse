e1dc6b04e6bf9ec4d3472ab4d92ccb705c66cd80
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-23 14:08:07 +0200

    javascript_course

--- section-7/script.js
+++ section-7/script.js
@@ -50,6 +50,17 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// The for-of Loop
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const item of menu) console.log(item);
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+// console.log([...menu.entries()]);
+
+///////////////////////////////////////
 // Coding Challenge #1
 
 /* 
