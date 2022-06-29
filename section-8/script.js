845eece15f415253d54d929fd0c8f2d6500a45d2
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-29 22:59:34 +0200

    javascript_course

--- section-8/script.js
+++ section-8/script.js
@@ -31,3 +31,38 @@
 
 ///////////////////////////////////////
 // How Passing Arguments Works: Values vs. Reference
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 23432432432432,
+// };
+
+// const checkIn = function (flightNumber, passenger) {
+//   flightNumber = 'LH9990';
+//   passenger.name = 'Mr.' + passenger.name;
+
+//   if (passenger.passport === 23432432432432) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // Is the same as doing
+// // const flightNumber = flight;
+// // const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+///////////////////////////////////////
+// Functions Accepting Callback Functions
