2dbcf8028a0b46d1b0172e81fc90054fd4d726ba
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-06-29 21:57:46 +0200

    javascript_course

--- section-8/script.js
+++ section-8/script.js
@@ -2,3 +2,32 @@
 
 ///////////////////////////////////////
 // Default Parameters
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNumber,
+//   numberOfPassangers = 1,
+//   price = 199 * numberOfPassangers
+// ) {
+//   // ES5
+//   // numberOfPassangers = numberOfPassangers || 1;
+//   // price = price || 199;
+
+//   const booking = {
+//     flightNumber,
+//     numberOfPassangers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+// createBooking('LH123', undefined, 1000);
+
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
