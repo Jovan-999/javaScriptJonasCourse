eac99a9cefb69b18487278c9786a3840db784ffe
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-02 14:59:33 +0200

    javascript_course

--- section-9/script.js
+++ section-9/script.js
@@ -192,6 +192,29 @@ btnTransfer.addEventListener('click', function (event) {
   }
 });
 
+btnClose.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    // .indexOf(23)
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
