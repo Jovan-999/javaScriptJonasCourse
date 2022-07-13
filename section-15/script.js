295c138bd625521b5cb2b519ca19552ec75d0ebe
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-13 11:40:24 +0200

    javascript_course

--- section-15/script.js
+++ section-15/script.js
@@ -81,3 +81,17 @@
 
 ///////////////////////////////////////
 // CommonJS Modules
+
+// Export
+//  export.addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//       `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+//     );
+//   };
+
+//   // Import
+//   const { addToCart } = require('./shoppingCart.js')
+
+///////////////////////////////////////
+// Introduction to NPM
