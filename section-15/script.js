41a99372f8e66caec2488d093569ed753ed3ac6f
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-13 11:17:23 +0200

    javascript_coures

--- section-15/script.js
+++ section-15/script.js
@@ -1,9 +1,12 @@
+///////////////////////////////////////
+// Exporting and Importing in ES6 Modules
+
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log('Importing module');
+// console.log('Importing module');
 // console.log(shippingCost) // cant do this
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -19,3 +22,27 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+//   // data.at(-1) last element
+// };
+// const lastPost = getLastPost();
+// console.log(lastPost);
+// // Not very clean
+// // lastPost.then(last => console.log(last));
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+///////////////////////////////////////
+// The Module Pattern
