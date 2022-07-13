540b7c4b1372b203111f3e9752d6e048419b058d
Jovan Milinkovic <milinkovicjovanbusiness@gmail.com>
2022-07-13 18:29:30 +0200

    javascript_course

---
+++ section-15/dist/index.3ec6c1be.js
@@ -0,0 +1,148 @@
+// var budget = [
+//   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+//   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+//   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+//   { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+//   { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+//   { value: -20, description: 'Candy 🍭', user: 'matilda' },
+//   { value: -125, description: 'Toys 🚂', user: 'matilda' },
+//   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+// ];
+// var limits = {
+//   jonas: 1500,
+//   matilda: 100,
+// };
+// var add = function (value, description, user) {
+//   if (!user) user = 'jonas';
+//   user = user.toLowerCase();
+//   var lim;
+//   if (limits[user]) {
+//     lim = limits[user];
+//   } else {
+//     lim = 0;
+//   }
+//   if (value <= lim) {
+//     budget.push({ value: -value, description: description, user: user });
+//   }
+// };
+// add(10, 'Pizza 🍕');
+// add(100, 'Going to movies 🍿', 'Matilda');
+// add(200, 'Stuff', 'Jay');
+// console.log(budget);
+// var check = function () {
+//   for (var el of budget) {
+//     var lim;
+//     if (limits[el.user]) {
+//       lim = limits[el.user];
+//     } else {
+//       lim = 0;
+//     }
+//     if (el.value < -lim) {
+//       el.flag = 'limit';
+//     }
+//   }
+// };
+// check();
+// console.log(budget);
+// var bigExpenses = function (limit) {
+//   var output = '';
+//   for (var el of budget) {
+//     if (el.value <= -limit) {
+//       output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+//     }
+//   }
+//   output = output.slice(0, -2); // Remove last '/ '
+//   console.log(output);
+// };
+// fixing bad old code to proffesional
+const budget = [
+    {
+        value: 250,
+        description: "Sold old TV \uD83D\uDCFA",
+        user: "jonas"
+    },
+    {
+        value: -45,
+        description: "Groceries \uD83E\uDD51",
+        user: "jonas"
+    },
+    {
+        value: 3500,
+        description: "Monthly salary \uD83D\uDC69\u200D\uD83D\uDCBB",
+        user: "jonas"
+    },
+    {
+        value: 300,
+        description: "Freelancing \uD83D\uDC69\u200D\uD83D\uDCBB",
+        user: "jonas"
+    },
+    {
+        value: -1100,
+        description: "New iPhone \uD83D\uDCF1",
+        user: "jonas"
+    },
+    {
+        value: -20,
+        description: "Candy \uD83C\uDF6D",
+        user: "matilda"
+    },
+    {
+        value: -125,
+        description: "Toys \uD83D\uDE82",
+        user: "matilda"
+    },
+    {
+        value: -1800,
+        description: "New Laptop \uD83D\uDCBB",
+        user: "jonas"
+    }, 
+];
+const spendingLimits = {
+    jonas: 1500,
+    matilda: 100
+};
+const getLimit = (user)=>spendingLimits?.[user] ?? 0;
+const addExpense = function(value, description, user = "jonas") {
+    user = user.toLowerCase();
+    // let lim;
+    // if (spendingLimits[user]) {
+    //   lim = spendingLimits[user];
+    // } else {
+    //   lim = 0;
+    // } ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
+    // const limit = spendingLimits?.[user] ?? 0 // same ⬇⬇⬇⬇⬆⬆⬆⬆
+    // const limit = getLimit(user);
+    if (value <= getLimit(user)) budget.push({
+        value: -value,
+        description: description,
+        user
+    });
+};
+addExpense(10, "Pizza \uD83C\uDF55");
+addExpense(100, "Going to movies \uD83C\uDF7F", "Matilda");
+addExpense(200, "Stuff", "Jay");
+const checkExpenses = function() {
+    // let lim;
+    // if (spendingLimits[entry.user]) {
+    //   lim = spendingLimits[entry.user];
+    // } else {
+    //   lim = 0;
+    // }
+    // const limit = getLimit(entry.user);
+    for (const entry of budget)if (entry.value < -getLimit(entry.user)) entry.flag = "limit";
+};
+checkExpenses();
+console.log(budget);
+const logBigExpenses = function(bigLimit) {
+    let output = "";
+    for (const entry of budget)output += entry.value <= bigLimit ? `${entry.description.slice(-2)} /` : "";
+    // if (entry.value <= -bigLimit) {
+    //   output += `${entry.description.slice(-2)} / `; // Emojis are 2 characters
+    // }
+    output = output.slice(0, -2); // Remove last '/ '
+    console.log(output);
+};
+console.log(budget);
+logBigExpenses(500);
+
+//# sourceMappingURL=index.3ec6c1be.js.map
